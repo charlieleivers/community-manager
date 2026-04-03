a4c896c7c163db51d9fa530ad174d5771fb60590
charlieleivers <73728614+charlieleivers@users.noreply.github.com>
2026-04-03 22:13:40 +0100

    fix: actually export auth and db

--- src/firebase.js
+++ src/firebase.js
@@ -18,8 +18,11 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
 
-const analytics = getAnalytics(app);
+// Initialize and export services
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
